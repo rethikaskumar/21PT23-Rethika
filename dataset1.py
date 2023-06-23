bdf8ef9b98861b031441dd8f5e6f76e7cca5426c
rethikaskumar <114384138+rethikaskumar@users.noreply.github.com>
2023-06-23 11:13:59 +0530

    Add files via upload

--- dataset1.py
+++ dataset1.py
@@ -8,6 +8,7 @@ Created on Fri Jun 23 08:16:01 2023
 from PIL import Image
 import json
 import pandas as pd
+from collections import Counter
 
 pixelList=[]
 lastrow=[]
@@ -22,23 +23,35 @@ dieHeight=inputJson['die']['height']
 
 careAreas=inputJson['care_areas']
 
-img=Image.open('wafer_image_1.png')
-px=img.load()
+for k in range(1, inputJson['die']['rows']*inputJson['die']['columns']+1):
+    img=Image.open('wafer_image_'+str(k)+'.png')
+    px=img.load()
+    
+    #print(px[0, 53])
+    #print(0%30)
+    
+    #careAreaWidth=careAreas[0]['bottom_right']['x']-careAreas[0]['top_left']['x']
+    #careAreaHeight=(dieHeight-careAreas[0]['top_left']['y']-1)-(dieHeight-careAreas[0]['bottom_right']['y']-1)
+#print(dieHeight-careAreas[0]['bottom_right']['y'])
+   
+    for j in range(dieHeight-careAreas[0]['top_left']['y'], dieHeight-careAreas[0]['bottom_right']['y']):
+         for i in range(careAreas[0]['top_left']['x'], careAreas[0]['bottom_right']['x']): 
+            #print(i, j)
+            if ((j)%30==0 and px[i, j]!=(255, 255, 255)):
+                pixelList.append([k, i, dieHeight-j-1])
+            elif ((j)%30!=0 and px[i, j]!=(128, 128, 128)):
+                pixelList.append([k, i, dieHeight-j-1])
+            if (dieHeight-j-1==0 and px[i, j]!=(128, 128, 128)):
+                print(k, i, dieHeight-j-1, px[i, j])
 
-print(px[0, 53])
-print(0%30)
 
 
-for i in range(careAreas[0]['bottom_right']['x']-careAreas[0]['top_left']['x']):
-    for j in range(careAreas[0]['top_left']['y']-careAreas[0]['bottom_right']['y']):
-        if (j%30==0 and px[i, j]!=(255, 255, 255)):
-            pixelList.append([i, dieHeight-j-1])
-        elif (j%30!=0 and px[i, j]!=(128, 128, 128)):
-            pixelList.append([i, dieHeight-j-1])
-        if (j==599 and px[i, j]!=(128, 128, 128)):
-            print(i, j, px[i, j])
-
-'''
 pixelDf=pd.DataFrame(pixelList)
 pixelDf.to_csv('pixel1.csv', index=False, header=False)
+
+'''
+#using counter
+for i in range(careAreaWidth):
+    for j in range(careAreaHeight):
+        count=Counter([px[i]])
 '''
